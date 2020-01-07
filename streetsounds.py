e48f9a60a2937ea29db0ea7a726f47741e9214c1
Brian Kaemingk <brian.e.k@gmail.com>
2020-01-06 16:51:46 -0800

    Initial commit

---
+++ streetsounds.py
@@ -0,0 +1,22 @@
+import kexp
+import pytz, datetime
+from datetime import timedelta
+###
+### A script that is run every Friday night to pull down the latest Street Sounds show from KEXP
+###
+if __name__ == '__main__':
+
+    #Get the date this is being run
+    utc_datetime = datetime.datetime.utcnow()
+
+    #Show starts at 5 UTC time
+    start_date = utc_datetime.strftime("%Y-%m-%dT05:00:00Z")
+
+    #Show ends at 8 UTC time
+    end_date = (datetime.datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%SZ") + timedelta(hours=3)).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    name = "Street Sounds on KEXP"
+    description = "Streetsounds is Seattle's longest running mixshow."
+    args = [name,start_date, end_date, description]
+    kexp.main(args)
+
