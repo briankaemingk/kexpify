2d9ca17bdb008b9bf5908e501fcf6776b0e0b676
Brian Kaemingk <brian.e.k@gmail.com>
2020-01-11 16:32:51 -0800

    Testing daily streetsounds

---
+++ createcache.py
@@ -0,0 +1,9 @@
+###
+### A script that creates the spotify auth cache file
+###
+
+import settings
+import os
+file = open(".cache-briankaemingk", "w") 
+file.write('{"token_type": "Bearer", "access_token": "' + os.environ.get("ACCESS_TOKEN") + '", "expires_in": 3600, "refresh_token": "' + os.environ.get('REFRESH_TOKEN') + '", "scope": "playlist-modify-private playlist-modify-public playlist-read-collaborative", "expires_at": 1578789444}') 
+file.close() 
